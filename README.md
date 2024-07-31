b22efc5c7243116733f0fe95b82a5bb9f25f8d9d
Niamh <niamh.linguist@gmail.com>
2024-07-31 14:18:31 +0300

    wrote up readme

--- README.md
+++ README.md
@@ -1,5 +1,37 @@
 # age-gender-recognition
 
-[Tutorial on YouTube](https://www.youtube.com/watch?v=ivFuOQcBiN8)
+## What
 
-[Repo with models](https://github.com/smahesh29/Gender-and-Age-Detection)
+Age and gender image recognition using Python, [OpenCV](https://pypi.org/project/opencv-python/) and deep learning models (more information [_here_](https://github.com/smahesh29/Gender-and-Age-Detection)).
+
+## Sources
+
+This was a practice Python project following a [tutorial on YouTube](https://www.youtube.com/watch?v=ivFuOQcBiN8) and using the code and models from this [repo](https://github.com/smahesh29/Gender-and-Age-Detection).
+
+## Disclaimer
+
+I claim no ownership over the code, models, or images used in this project.
+
+## Running
+
+Clone repo and run:
+
+```
+pip install opencv-python
+```
+
+to install the single dependency OpenCV and then run:
+
+```
+python main.py
+```
+
+to run the program.
+
+## Example results
+
+![Example of results 1](/imgs/readme/results_1.png)
+
+![Example of results 2](/imgs/readme/results_2.png)
+
+![Example of results 3](/imgs/readme/results_3.png)
--- main.py
+++ main.py
@@ -1,23 +1,5 @@
 import cv2
 
-# Read Image
-image = cv2.imread("imgs/group_2.png")
-img = cv2.resize(image, (640, 720))
-
-# define models
-face_pbtxt = "models/opencv_face_detector.pbtxt"
-face_pb = "models/opencv_face_detector_uint8.pb"
-age_prototxt = "models/age_deploy.prototxt"
-age_model = "models/age_net.caffemodel"
-gender_prototxt = "models/gender_deploy.prototxt"
-gender_model = "models/gender_net.caffemodel"
-MODEL_MEAN_VALUES = (78.4263377603, 87.7689143744, 114.895847746)
-
-# load models
-face = cv2.dnn.readNet(face_pb, face_pbtxt)
-age = cv2.dnn.readNet(age_model, age_prototxt)
-gender = cv2.dnn.readNet(gender_model, gender_prototxt)
-
 # setup classifications
 age_classifications = [
     "(0-2 yrs)",
@@ -31,7 +13,87 @@ age_classifications = [
 ]
 gender_classifications = ["Male", "Female"]
 
+# define models
+face_pbtxt = "models/opencv_face_detector.pbtxt"
+face_pb = "models/opencv_face_detector_uint8.pb"
+age_prototxt = "models/age_deploy.prototxt"
+age_model = "models/age_net.caffemodel"
+gender_prototxt = "models/gender_deploy.prototxt"
+gender_model = "models/gender_net.caffemodel"
+
+# Constants
 PADDING = 15
+MODEL_MEAN_VALUES = (78.4263377603, 87.7689143744, 114.895847746)
+FACES_DETECTION_THRESHOLD = 0.85
+
+# load models
+face = cv2.dnn.readNet(face_pb, face_pbtxt)
+age = cv2.dnn.readNet(age_model, age_prototxt)
+gender = cv2.dnn.readNet(gender_model, gender_prototxt)
+
+# draw rectangle over faces
+
+
+def draw_rectangle(faces):
+    list = []
+    for i in range(faces.shape[2]):
+        confidence = faces[0, 0, i, 2]
+        if confidence > FACES_DETECTION_THRESHOLD:
+            x1 = int(faces[0, 0, i, 3] * img_w)
+            y1 = int(faces[0, 0, i, 4] * img_h)
+            x2 = int(faces[0, 0, i, 5] * img_w)
+            y2 = int(faces[0, 0, i, 6] * img_h)
+            cv2.rectangle(
+                img_cp, (x1, y1), (x2, y2), (0, 255, 0), int(round(img_h / 150)), 8
+            )
+            list.append([x1, y1, x2, y2])
+
+    if not list:
+        print("No faces detected")
+        exit()
+    return list
+
+
+def extract_face(face_bound, img):
+    face = img[
+        max(0, face_bound[1] - PADDING) : min(
+            face_bound[3] + PADDING, img.shape[0] - 1
+        ),
+        max(0, face_bound[0] - PADDING) : min(
+            face_bound[2] + PADDING, img.shape[1] - 1
+        ),
+    ]
+    return cv2.dnn.blobFromImage(face, 1.0, (227, 227), MODEL_MEAN_VALUES, True)
+
+
+def generate_gender_str(blob):
+    gender.setInput(blob)
+    gender_prediction = gender.forward()
+    return gender_classifications[gender_prediction[0].argmax()]
+
+
+def generate_age_str(blob):
+    age.setInput(blob)
+    age_prediction = age.forward()
+    return age_classifications[age_prediction[0].argmax()]
+
+
+def apply_text(face_bound, img, gender_str, age_str):
+    cv2.putText(
+        img,
+        f"{gender_str} {age_str}",
+        (face_bound[0], face_bound[1] - PADDING),
+        cv2.FONT_HERSHEY_SIMPLEX,
+        0.8,
+        (0, 0, 255),
+        2,
+        cv2.LINE_AA,
+    )
+
+
+# Read Image
+image = cv2.imread("imgs/sources/woman_1.jpg")
+img = cv2.resize(image, (640, 720))
 
 # Copy img
 img_cp = img.copy()
@@ -44,56 +106,15 @@ blob = cv2.dnn.blobFromImage(img_cp, 1.0, (300, 300), [104, 117, 123], True, Fal
 face.setInput(blob)
 detected_faces = face.forward()
 
-face_bounds = []
-
-# draw rectangle over faces
-
-for i in range(detected_faces.shape[2]):
-    confidence = detected_faces[0, 0, i, 2]
-    if confidence > 0.6:
-        x1 = int(detected_faces[0, 0, i, 3] * img_w)
-        y1 = int(detected_faces[0, 0, i, 4] * img_h)
-        x2 = int(detected_faces[0, 0, i, 5] * img_w)
-        y2 = int(detected_faces[0, 0, i, 6] * img_h)
-        cv2.rectangle(
-            img_cp, (x1, y1), (x2, y2), (0, 255, 0), int(round(img_h / 150)), 8
-        )
-        face_bounds.append([x1, y1, x2, y2])
-
-if not face_bounds:
-    print("No faces detected")
-    exit()
+# LIST OF FACES
+face_bounds = draw_rectangle(detected_faces)
 
 for face_bound in face_bounds:
     try:
-        face = img_cp[
-            max(0, face_bound[1] - PADDING) : min(
-                face_bound[3] + PADDING, img_cp.shape[0] - 1
-            ),
-            max(0, face_bound[0] - PADDING) : min(
-                face_bound[2] + PADDING, img_cp.shape[1] - 1
-            ),
-        ]
-        blob = cv2.dnn.blobFromImage(face, 1.0, (227, 227), MODEL_MEAN_VALUES, True)
-
-        gender.setInput(blob)
-        gender_prediction = gender.forward()
-        gender_str = gender_classifications[gender_prediction[0].argmax()]
-
-        age.setInput(blob)
-        age_prediction = age.forward()
-        age_str = age_classifications[age_prediction[0].argmax()]
-
-        cv2.putText(
-            img_cp,
-            f"{gender_str} {age_str}",
-            (face_bound[0], face_bound[1] - PADDING),
-            cv2.FONT_HERSHEY_SIMPLEX,
-            0.8,
-            (0, 0, 255),
-            2,
-            cv2.LINE_AA,
-        )
+        blob = extract_face(face_bound, img_cp)
+        gender_str = generate_gender_str(blob)
+        age_str = generate_age_str(blob)
+        apply_text(face_bound, img_cp, gender_str, age_str)
 
     except Exception as e:
         print(e)
